c645c3e31eac4a415671f772fe83cd625d14420c
Besslan <besslan.bazadough@gmail.com>
2019-05-08 14:14:52 +0300

    Create Feature Request Model

--- app/models.py
+++ app/models.py
@@ -19,3 +19,16 @@ class ProductArea(db.Model):
 
     def __repr__(self):
             return 'name: %s' % (self.name)
+
+
+class FeatureRequest(db.Model):
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(50), nullable=False)
+    description = Column(String(1500), nullable=False)
+    target_date = Column(DateTime, default=datetime.utcnow, nullable=False)
+    client_priority = Column(Integer, nullable=False )
+  
+    def __repr__(self):
+
+            return 'title: %s  description: %s target_date: %s client_priority: %s client_id: %s product_area_id %s' % (self.title, self.description, self.target_date, self.client_priority, self.client_id, self.product_area_id )
