87c97ae4dc5bf6cb6357754549fd238b92059530
Besslan <besslan.bazadough@gmail.com>
2019-05-08 14:12:08 +0300

    Create Client Model

---
+++ app/models.py
@@ -0,0 +1,13 @@
+from app import db
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from datetime import datetime
+
+
+class Client(db.Model):
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+
+    def __repr__(self):
+            return 'name: %s' % (self.name)
+
