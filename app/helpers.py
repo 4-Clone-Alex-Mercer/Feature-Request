3377f82a46eadeebc7e356dc1cc586c602bb3d8f
Besslan <besslan.bazadough@gmail.com>
2019-05-08 16:47:36 +0300

    Add New Feature Request To DB

--- app/helpers.py
+++ app/helpers.py
@@ -8,3 +8,21 @@ def getRequests():
     for request in requests:
         requestsList.append(request.serialize)
     return requestsList
+ 
+
+def getClientName(name):
+    return Client.query.filter(Client.name == name).first().id
+    
+
+def getAreaName(name):
+    return ProductArea.query.filter(ProductArea.name == name).first().id
+
+
+def addFeatueRequest(req):
+    clientId = str(getClientName(req['client']))
+    areaId = str(getAreaName(req['product_area']))
+    featureRequest = FeatureRequest(title=req['title'], description=req['description'],
+                             target_date=req['target_date'], client_priority=req['client_priority'], client_id=clientId, product_area_id=areaId)
+    db.session.add(featureRequest)
+    db.session.commit()
+    return req
